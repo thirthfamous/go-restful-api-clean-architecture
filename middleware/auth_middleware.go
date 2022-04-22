7c99e5d83c65b19ba0584b56c2111f0e20e96e2c
Farhan Hamdallah <fhbluebeast356@gmail.com>
2022-04-22 13:10:30 +0700

    first commit

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"net/http"
+	"thirthfamous/golang-restful-api-clean-architecture/helper"
+	"thirthfamous/golang-restful-api-clean-architecture/model/web"
+)
+
+type AuthMiddleware struct {
+	Handler http.Handler
+}
+
+func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
+	return &AuthMiddleware{Handler: handler}
+}
+
+func (middleware AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if request.Header.Get("X-API-Key") == "RAHASIA" {
+		// ok
+		middleware.Handler.ServeHTTP(writer, request)
+	} else {
+		// error
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusUnauthorized)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusUnauthorized,
+			Status: "UNAUTHORIZED",
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+	}
+}
